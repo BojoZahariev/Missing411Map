f7c95dcc80c107ff1c70bdd81bb5678d612e8d44
Bojo <bozhidar_zahariev@hotmail.com>
2020-09-22 17:15:40 +0100

    2 markers

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([51.505, -0.09], 6);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution:
