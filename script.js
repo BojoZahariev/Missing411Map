936e32d198807f625500aecd4c6fbdff35bf2cbc
Bojo <bozhidar_zahariev@hotmail.com>
2020-09-28 09:20:00 +0100

    objects

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 6);
+var mymap = L.map('mapid').setView([42.442, -108.46], 4);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution:
@@ -10,10 +10,20 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: 'pk.eyJ1IjoiYm9qbzE5ODAiLCJhIjoiY2tmZTF1YTZ3MDBvejJ1cXZ5MnRsMmFlbyJ9.Oj8H9UBmf69qLHjmT2RlJw'
 }).addTo(mymap);
 
-var mark = L.marker([51.5, -0.09])
-  .addTo(mymap)
-  .bindPopup('This is 1');
+let m1 = {
+  coordinates: [42.442, -108.468],
+  text: 'This is 1'
+};
 
-var mark2 = L.marker([52.7, -0.09])
+let m2 = {
+  coordinates: [38.57, -96.71],
+  text: 'This is 2'
+};
+
+var mark = L.marker(m1.coordinates)
   .addTo(mymap)
-  .bindPopup('This is 2');
+  .bindPopup(m1.text);
+
+var mark2 = L.marker(m2.coordinates)
+  .addTo(mymap)
+  .bindPopup(m2.text);
